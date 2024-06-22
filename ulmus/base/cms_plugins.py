d519675ef29db4f76f4fd035eb0e09046d9a21c7
YessineBR <ben.rhouma.yessine0610@gmail.com>
2024-06-22 14:19:19 +0100

    Created .gitignore file and added base app to git tracking

--- ulmus/base/cms_plugins.py
+++ ulmus/base/cms_plugins.py
@@ -1,4 +1,13 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import gettext_lazy as _
-from .models import HeroSection, AboutSection
+from .models import HeroSection
+
+
+@plugin_pool.register_plugin
+class HeroSectionPlugin(CMSPluginBase):
+    model = HeroSection
+    render_template = "base/hero_section.html"
+    cache = False
+    module = _("Théme Ulmus")
+    name = _("Hero Section")
--- ulmus/base/models.py
+++ ulmus/base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from cms.models.pluginmodel import CMSPlugin
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class HeroSection(CMSPlugin):
+    title = models.CharField(max_length=255, verbose_name=_("Titre"))
+    subtitle = models.CharField(max_length=255, verbose_name=_("Sous-titre"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Date de création"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Date de modification"))
+    
+    class Meta:
+        get_latest_by = 'created_at'
+    
+    def __str__(self):
+        return f"Hero Section"
