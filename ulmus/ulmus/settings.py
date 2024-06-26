f3b3dabf67946c931f587cb8adf9b53e94ab2c3e
YessineBR <ben.rhouma.yessine0610@gmail.com>
2024-06-26 12:46:51 +0100

    preparing for PostgreSQL migration

--- ulmus/ulmus/settings.py
+++ ulmus/ulmus/settings.py
@@ -134,9 +134,17 @@ WSGI_APPLICATION = 'ulmus.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ulmus',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
